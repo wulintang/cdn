ae4054120e46e71158b1061e5e124cce99d211b2
南通伍林堂文化传播有限公司 <15062733214@qq.com>
2022-06-25 09:11:37 +0800

    Update taiji.js

--- taiji/taiji.js
+++ taiji/taiji.js
@@ -53,4 +53,99 @@ document.writeln("	</div>");
 document.writeln("</div>");
 $(document).ready(function(){
 	/* ----- 侧边悬浮 ---- */
-	$(document)
+	$(document).on("mouseenter", ".suspension .a", function(){
+		var _this = $(this);
+		var s = $(".suspension");
+		var isService = _this.hasClass("a-service");
+		var isServicePhone = _this.hasClass("a-service-phone");
+		var isQrcode = _this.hasClass("a-qrcode");
+		if(isService){ s.find(".d-service").show().siblings(".d").hide();}
+		if(isServicePhone){ s.find(".d-service-phone").show().siblings(".d").hide();}
+		if(isQrcode){ s.find(".d-qrcode").show().siblings(".d").hide();}
+	});
+	$(document).on("mouseleave", ".suspension, .suspension .a-top", function(){
+		$(".suspension").find(".d").hide();
+	});
+	$(document).on("mouseenter", ".suspension .a-top", function(){
+		$(".suspension").find(".d").hide(); 
+	});
+	$(document).on("click", ".suspension .a-top", function(){
+		$("html,body").animate({scrollTop: 0});
+	});
+	$(window).scroll(function(){
+		var st = $(document).scrollTop();
+		var $top = $(".suspension .a-top");
+		if(st > 400){
+			$top.css({display: 'block'});
+		}else{
+			if ($top.is(":visible")) {
+				$top.hide();
+			}
+		}
+	});
+	
+});	
+
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    //点击body时触发事件
+    $("body").click(function(e) {
+    //需要显示的词语
+    var a = new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");
+    //设置词语给span标签
+    var $i = $("<span/>").text(a[a_idx]);
+    //下标等于原来下标+1  余 词语总数
+    a_idx = (a_idx + 1)% a.length;
+    //获取鼠标指针的位置，分别相对于文档的左和右边缘。
+    //获取x和y的指针坐标
+    var x = e.pageX, y = e.pageY;
+    //在鼠标的指针的位置给$i定义的span标签添加css样式
+    $i.css({"z-index" : 999999999999999999999999999999999999999999999999999999999999999999999,
+        "top" : y - 20,
+        "left" : x,
+        "position" : "absolute",
+        "font-size": "16px!important",
+        "font-weight" : "bold",
+        "color" : "#D14836"
+        });
+    //在body添加这个标签
+    $("body").append($i);
+        //animate() 方法执行 CSS 属性集的自定义动画。
+        //该方法通过CSS样式将元素从一个状态改变为另一个状态。CSS属性值是逐渐改变的，这样就可以创建动画效果。
+        //详情请看http://www.w3school.com.cn/jquery/effect_animate.asp
+        $i.animate({
+        //将原来的位置向上移动180
+            "top" : y - 180,
+                "opacity" : 0
+         //1500动画的速度
+        }, 1500, function() {
+        //时间到了自动删除
+            $i.remove();
+        });
+    });
+})
+;
+document.writeln("<!-- 灯笼 1 -->");
+document.writeln("<div class=\'deng-box\'>");
+document.writeln("<div class=\'deng\'>");
+document.writeln("<div class=\'xian\'></div>")
+document.writeln("<div class=\'deng-a\'>");
+document.writeln("<div class=\'deng-b\'><div class=\'deng-t\'>节</div></div>");
+document.writeln("</div>");
+document.writeln("<div class=\'shui shui-a\'></div>");
+document.writeln("<div class=\'shui shui-b\'></div>");
+document.writeln("<div class=\'shui shui-c\'></div>");
+document.writeln("</div>");
+document.writeln("</div>");
+document.writeln("<!-- 灯笼 2 -->");
+document.writeln("<div class=\'deng-box1\'>");
+document.writeln("<div class=\'deng\'>");
+document.writeln("<div class=\'xian\'></div>")
+document.writeln("<div class=\'deng-a\'>");
+document.writeln("<div class=\'deng-b\'><div class=\'deng-t\'>春</div></div>");
+document.writeln("</div>");
+document.writeln("<div class=\'shui shui-a\'></div>");
+document.writeln("<div class=\'shui shui-b\'></div>");
+document.writeln("<div class=\'shui shui-c\'></div>");
+document.writeln("</div>");
+document.writeln("</div>");
