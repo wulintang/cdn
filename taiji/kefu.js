dd8c536c4da6045521dc2571798c6ddb7a18c207
南通伍林堂文化传播有限公司 <15062733214@qq.com>
2023-06-20 22:35:55 +0800

    Update kefu.js

--- taiji/kefu.js
+++ taiji/kefu.js
@@ -1,38 +1,39 @@
-const qqClientTemplate = `
-<div class="qq-client">
-    <a href="javascript:void(0);" class="qq-client-open"><p>点</p><p>击</p><p>展</p><p>开</p></a>
-    <div class="qq-client-content">
-        <h1>功能中心<span class="qq-client-close">关闭</span></h1>
-        <div class="qq-client-list">
-            <div class="client-list"><a href="https://www.wulintang.cn/bbs/fid/2/" target="_blank">意见建议</a> </div>
-            <div class="client-list"><a href="https://www.wulintang.cn/bbs/fid/1/" target="_blank">问题反馈</a> </div>
-            <div class="client-list"><a href="mailto:jubao@ccwadj.cn">投诉举报</a> </div>
-            <div class="client-list"><a href="http://wpa.qq.com/msgrd?v=3&uin=466660026&site=qq&menu=yes" target="_blank">企鹅在线</a> </div>
-            <div class="client-list"><a href="tel:1234567890">联系电话</a> </div>
-            <div class="client-list"><a href="#" id="back-to-top" title="返回顶部">返回顶部</a>  </div>
-        </div>
-    </div>
-</div>
+const cssString = `
+.qq-client{position:fixed;right:0px;top:50%;margin-top:-80px;}
+.qq-client a{width:50px;height:160px;text-align:center;border:#ebebeb solid 1px;padding:0px 0px;line-height:40px;display:block;background-color: #fff;}
+.qq-client-content{position:fixed;right:-250px;top:38%;border:#ebebeb solid 1px;width:10%;display:none;background:#ffffff;}
+.qq-client-content h1{font-size:14px;width:90%;margin:0px auto;text-align:center;height:50px;line-height:50px;border-bottom:#ebebeb solid 1px;position:relative;}
+.qq-client-content h1 span{font-size:12px;font-weight:normal;position:absolute;left:-30px;top:-20px;cursor:pointer;background:#ffffff;border:#ebebeb solid 1px;width:40px;height:40px;line-height:40px;border-radius:20px;text-align:center;}
+.qq-client-list{background:#ffffff;}
+.client-list{overflow: hidden;line-height: 300%;width: 100%;margin: 0px auto;border-bottom: dashed 1px #ebebeb;text-align: center;}
+.client-list a{display:block;}
+.client-list a:hover{color:red;}
+.client-list span{float:left;}
+.client-list label{float:left;width:60px;text-align:right;padding-right:10px;}
 `;
-
-$('body').append(qqClientTemplate);
 
 $(function(){
     // 将CSS插入到页面中
-    const cssString = `
-    .qq-client{position:fixed;right:0px;top:50%;margin-top:-80px;}
-    .qq-client a{width:50px;height:160px;text-align:center;border:#ebebeb solid 1px;padding:0px 0px;line-height:40px;display:block;background-color: #fff;}
-    .qq-client-content{position:fixed;right:-250px;top:38%;border:#ebebeb solid 1px;width:10%;display:none;background:#ffffff;}
-    .qq-client-content h1{font-size:14px;width:90%;margin:0px auto;text-align:center;height:50px;line-height:50px;border-bottom:#ebebeb solid 1px;position:relative;}
-    .qq-client-content h1 span{font-size:12px;font-weight:normal;position:absolute;left:-30px;top:-20px;cursor:pointer;background:#ffffff;border:#ebebeb solid 1px;width:40px;height:40px;line-height:40px;border-radius:20px;text-align:center;}
-    .qq-client-list{background:#ffffff;}
-    .client-list{overflow: hidden;line-height: 300%;width: 100%;margin: 0px auto;border-bottom: dashed 1px #ebebeb;text-align: center;}
-    .client-list a{display:block;}
-    .client-list a:hover{color:red;}
-    .client-list span{float:left;}
-    .client-list label{float:left;width:60px;text-align:right;padding-right:10px;}
-    `;
     $('<style>').text(cssString).appendTo('head');
+
+    const qqClientTemplate = `
+    <div class="qq-client">
+        <a href="javascript:void(0);" class="qq-client-open"><p>点</p><p>击</p><p>展</p><p>开</p></a>
+        <div class="qq-client-content">
+            <h1>功能中心<span class="qq-client-close">关闭</span></h1>
+            <div class="qq-client-list">
+                <div class="client-list"><a href="https://www.wulintang.cn/bbs/fid/2/" target="_blank">意见建议</a> </div>
+                <div class="client-list"><a href="https://www.wulintang.cn/bbs/fid/1/" target="_blank">问题反馈</a> </div>
+                <div class="client-list"><a href="mailto:jubao@ccwadj.cn">投诉举报</a> </div>
+                <div class="client-list"><a href="http://wpa.qq.com/msgrd?v=3&uin=466660026&site=qq&menu=yes" target="_blank">企鹅在线</a> </div>
+                <div class="client-list"><a href="tel:1234567890">联系电话</a> </div>
+                <div class="client-list"><a href="#" id="back-to-top" title="返回顶部">返回顶部</a>  </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+    $('body').append(qqClientTemplate);
 
     H_qqServer = {};
     H_qqServer.clickOpenServer = function () {
@@ -52,7 +53,7 @@ $(function(){
             }, 400);
             $('.qq-client-content').animate({
                 right: '-250',
-                opacity: 'show'
+                opacity: 'hide' // 将此处的'show'改为'hide'
             }, 800);
         });
     };
