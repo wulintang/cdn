56b4bbbc04bc5893446711dcab71ab8e5908d6a5
懋和道人 <15062733214@qq.com>
2023-09-13 13:11:54 +0800

    Update tao.js

--- daohang/tao.js
+++ daohang/tao.js
@@ -1,8 +1,8 @@
 // 淘宝优惠券 js调用
 document.writeln("<div id=\'img1\' style=\'position: fixed; margin-top: 200px; margin-left: 80px; top: 0px;visibility: visible;\'>");
 document.writeln("<a href=\'https://www.wulintang.cn/youhui\' target=\'_blank\'>");
-document.writeln("<img src=\'https://www.abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba.cn/daohang/html/images/gou.gif\' />");
+document.writeln("<img src=\'https://cdn.jsdelivr.net/gh/wulintang/cdn@1.0/daohang/html/images/gou.gif\' />");
 document.writeln("</a>");
 document.writeln("</div>");
 document.writeln("<style>@media(max-width:960px){#img1{display: none;}}</style>");
-document.writeln("<script type=\'text/javascript\' src=\'https://www.abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba.cn/daohang/lik.js\' charset=\'UTF-8\'></script>");
+document.writeln("<script type=\'text/javascript\' src=\'https://cdn.jsdelivr.net/gh/wulintang/cdn@1.0/daohang/lik.js\' charset=\'UTF-8\'></script>");
