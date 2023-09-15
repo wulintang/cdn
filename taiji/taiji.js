9a94363028e4b04b063ab182c8fc60821d614fb4
懋和道人 <15062733214@qq.com>
2023-09-15 09:06:38 +0800

    Update taiji.js

--- taiji/taiji.js
+++ taiji/taiji.js
@@ -1,5 +1,5 @@
 // 这个js建议放在body第一行
-document.body.innerHTML += ('<div id="loader-wrapper"><div id="loader"></div><div class="divClass"><div class="taijileft"></div><div class="taijiright"></div></div><div id="load_title"><p><img style="width: 200px!important;" src="https://cdn.jsdelivr.net/gh/wulintang/cdn@1.0/taiji/login_logo.png"/></p><p>正在加载中，请稍后……</p></div></div>');
+document.body.innerHTML += ('<div id="loader-wrapper"><div id="loader"></div><div class="divClass"><div class="taijileft"></div><div class="taijiright"></div></div><div id="load_title"><p><img style="width: 200px!important;" src="https://cdn.jsdelivr.net/gh/wulintang/cdn/taiji/login_logo.png"/></p><p>正在加载中，请稍后……</p></div></div>');
 window.onload = function() {
     if (document.body.className == '' || document.body.className.search("loaded") == -1)
         document.body.className += ' loaded'
